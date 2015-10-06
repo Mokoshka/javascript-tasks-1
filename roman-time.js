9bce142da5eea019633e1479388da8e2d5522e8d
Rybalka Ekaterina <lonelygopnik@gmail.com>
2015-10-06 15:30:30 +0500

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -12,6 +12,9 @@ function toRimSimbol(arabSimbol){
         case(3): return "III";
         case(4): return "IV";
         case(5): return "V";
+        case(6): return "VI";
+        case(7): return "VII";
+        case(8): return "VIII";
         case(9): return "IX";
         case(10): return "X";
         case(20): return "XX";
